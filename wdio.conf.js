4a1b2c4c28e20e3f6a66a4156cb4759e836bee7b
David <3dgiordano@gmail.com>
2017-11-08 00:11:46 -0300

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,6 +23,7 @@ exports.config = {
         deviceOrientation: 'portrait',
         //clearSystemFiles: false
         deviceReadyTimeout: 60,
+        androidDeviceReadyTimeout: 60, 
         commandTimeout: 600,
         idleTimeout: 600,
         maxDuration: 1800
