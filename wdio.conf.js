1bb552448bd9f463de167f34bb5d98b4d073fd3e
David <3dgiordano@gmail.com>
2017-11-08 10:40:08 -0300

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -55,7 +55,7 @@ exports.config = {
     services: ['sauce'],
     user: process.env.SAUCE_USERNAME,
     key: process.env.SAUCE_ACCESS_KEY,
-    sauceConnect: true,
+    sauceConnect: false,
     //host: 'ondemand.saucelabs.com',
     sauceConnectOpts: {
         verboseDebugging: true,
